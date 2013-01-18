2ec6b38bacb919c8f0006f880757632fec885107
Greg Allen <me@jga.me>
2013-01-17 16:32:06 -0800

    [dist] renamed jquery.fit to fit

--- dist/fit.js
+++ dist/fit.js
@@ -1,10 +1,10 @@
 /*!
-  * jquery.fit.js - A jQuery plugin that will center and crop images inside a container 
-  * v0.2.0
-  * https://github.com/jgallen23/fit
-  * copyright JGA 2012
-  * MIT License
-  */
+ * fit - A plugin to fit images inside a container
+ * v0.2.0
+ * https://github.com/jgallen23/fit
+ * copyright Greg Allen 2013
+ * MIT License
+*/
 
 (function($) {
 
